a08b528d01f55b47706f30a0b9f1a3aeb2325866
Quyen Tat <tvq9612@gmail.com>
2022-07-30 09:34:40 +0700

    🚀

---
+++ index.js
@@ -0,0 +1,34 @@
+const fetch = require("node-fetch");
+const fs = require("fs");
+
+const getPrice = async () => {
+  try {
+    const today = new Date().toISOString().slice(0, 10);
+    const response = await fetch(
+      "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
+    );
+    const data = await response.json();
+    const price = parseFloat(data.price).toFixed(2);
+
+    console.log("today", today);
+    console.log("price", price);
+
+    return {
+      today,
+      price,
+    };
+  } catch (err) {
+    console.error(err.message);
+    return {};
+  }
+};
+
+const generate = async () => {
+  const { today, price } = await getPrice();
+
+  if (!price) return;
+
+  fs.appendFileSync("README.md", `| ${today} | ${price} |`);
+};
+
+generate();
